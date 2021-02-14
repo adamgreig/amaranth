54e79001d9a4c298b02a0e16c3e4746d4d9b91bb
Adam Greig <adam@adamgreig.com>
2021-02-14 18:00:16 +0000

    use autoapi to generate docs

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,9 @@ extensions = [
     "sphinx.ext.todo",
     "sphinx_rtd_theme",
     "sphinxcontrib.platformpicker",
+    "autoapi.extension",
+    "sphinx.ext.napoleon",
+    "numpydoc",
 ]
 
 with open(".gitignore") as f:
@@ -28,3 +31,6 @@ todo_include_todos = True
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["custom.css"]
+
+autoapi_type = 'python'
+autoapi_dirs = ['../nmigen']
